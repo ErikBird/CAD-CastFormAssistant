74e2936cfc3ed16e4c78687c458d16ca13dc7585
ErikBird <github@erikschwan.de>
2022-12-08 16:49:16 +0100

    refactor(timeline): Improve Timteline Style

--- frontend/src/views/production/Timeline.vue
+++ frontend/src/views/production/Timeline.vue
@@ -1,22 +1,22 @@
 <template>
-  <v-navigation-drawer floating permanent location="right">
-    <v-timeline align="start" side="end">
-      <v-timeline-item :dot-color="get_color(0)">
+  <v-navigation-drawer floating permanent location="right" class="mt-10">
+    <v-timeline align="start" side="end" truncate-line="both">
+      <v-timeline-item :dot-color="get_color(0)" fill-dot>
         Datei Hochladen
       </v-timeline-item>
-      <v-timeline-item :dot-color="get_color(1)">
+      <v-timeline-item :dot-color="get_color(1)" fill-dot>
         Form Erstellen
       </v-timeline-item>
-      <v-timeline-item :dot-color="get_color(2)">
+      <v-timeline-item :dot-color="get_color(2)" fill-dot>
         Flächen Markieren
       </v-timeline-item>
-      <v-timeline-item :dot-color="get_color(3)">
+      <v-timeline-item :dot-color="get_color(3)" fill-dot>
         Perforierung Einstellen
       </v-timeline-item>
-      <v-timeline-item :dot-color="get_color(4)">
+      <v-timeline-item :dot-color="get_color(4)" fill-dot>
         Anschluss Markieren
       </v-timeline-item>
-      <v-timeline-item :dot-color="get_color(5)">
+      <v-timeline-item :dot-color="get_color(5)" fill-dot>
         Download
       </v-timeline-item>
     </v-timeline>
